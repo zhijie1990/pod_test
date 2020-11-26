7fde221e9b151b25e766530e7bca105074adacd0
zhaojun <zhaojun1990@126.com>
2020-11-26 16:22:46 +0800

    test

--- pod_test/Classes/NSString+Test.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSString (Test)
-- (void)test;
-@end
--- pod_test/Classes/NSString+Test.m
+++
@@ -1,7 +0,0 @@
-#import "NSString+Test.h"
-
-@implementation NSString (Test)
-- (void)test{
-    NSLog(@"只是一个测试");
-}
-@end
